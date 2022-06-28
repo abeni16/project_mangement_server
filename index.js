819428462bc8830ce2ff724662c57a2c2ec27429
abeni16 <abeni3ase7@gmail.com>
2022-06-28 22:03:01 +0300

    client and project querys

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const colors = require("colors");
+
+const app = express();
+const connectDB = require("./config/db");
+require("dotenv").config();
+const schema = require("./schema/schema");
+const { graphqlHTTP } = require("express-graphql");
+
+const port = process.env.PORT || 5000;
+connectDB();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(port, console.log(`server is runnig on port ${process.env.PORT}`));
